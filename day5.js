52d9454a2763c3b62ab7abb533ddfdcb8660f056
YounesM <younes@mgharfaoui.com>
2016-12-05 22:08:46 +0000

    Finished day 5

--- day5.js
+++ day5.js
@@ -1,8 +1,9 @@
 /*
      Day 5: How About a Nice Game of Chess?
+     Status: SUCCEEDED
  */
 
-
+// PART 1
 // npm install blueimp-md5
 var md5 = require("blueimp-md5");
 var doorID = "uqwqemis";
@@ -18,9 +19,38 @@ while(res.length < 8){
     }
 
     if(zeros >= 5){
-        console.log("!!!!!! FOUND !!!!!!");
-        res += hash.charAt(5);
+        if(res.length < 8){
+            res += hash.charAt(5);
+        }
     }
     code++;
 }
-console.log("Code : " + res);
+console.log("Code PART 1: " + res);
+
+//PART 2
+/*
+    I'm redoing the work done above here instead of simply adding lines to the PART 1's while loop
+    so that PART 1 and PART 2 can become distinct.
+ */
+
+var arRes = ["","","","","","","",""];
+code = 0;
+
+while(arRes.toString().length < 15){
+    hash = md5(doorID + code);
+    zeros = 0;
+
+    for(i=0 ; i < 5 ; i++){
+        hash.charAt(i)=="0"?zeros++:0;
+    }
+
+    if(zeros >= 5){
+        if(parseInt(hash.charAt(5)) < 8){
+            if(arRes[hash.charAt(5)] == ""){
+                arRes[hash.charAt(5)] = hash.charAt(6);
+            }
+        }
+    }
+    code++;
+}
+console.log("Code PART 2: " + arRes);
