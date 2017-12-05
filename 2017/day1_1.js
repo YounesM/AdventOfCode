d837ed64495eef9d184fefb26a0961b14cea6fe7
YounesM <younes@mgharfaoui.com>
2017-12-05 10:36:43 +0000

    Day 1 shorter

--- 2017/day1_1.js
+++ 2017/day1_1.js
@@ -2,18 +2,12 @@
 //
 
 const fs = require('fs');
-let input = fs.readFileSync('input/2017/day1','utf8');
+let input = fs.readFileSync('input/2017/day1', 'utf8');
 let sum = 0;
-for(let i=0; i < input.split('').length; i++){
-    if(i < input.length-1) {
-        if(input[i] === input[i+1]){
-            sum += parseInt(input[i]);
-        }
-    } else {
-        if(input[i] === input[0]){
-            sum += parseInt(input[i]);
-        }
+for (let i = 0; i < input.split('').length; i++) {
+    if (input[i] === input[(i+1)% input.length]) {
+        sum += parseInt(input[i]);
     }
 }
 
-console.log('PART 1: '+sum);
+console.log('PART 1: ' + sum);
