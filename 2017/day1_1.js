ba56d645aba0ea91c6619715b59cb1fd59653944
YounesM <younes@mgharfaoui.com>
2017-12-05 13:44:24 +0000

    Reformatting

--- 2017/day1_1.js
+++ 2017/day1_1.js
@@ -1,7 +1,6 @@
 // --- Day 1: Inverse Captcha (1/2)---
 const fs = require('fs');
-let input = fs.readFileSync('input/2017/day1', 'utf8');
-let sum = 0;
+let input = fs.readFileSync('input/2017/day1', 'utf8'), sum = 0;
 for (let i = 0; i < input.split('').length; i++) {
     if (input[i] === input[(i + 1) % input.length]) {
         sum += parseInt(input[i]);
