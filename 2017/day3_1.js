0c2ae04372dd392d7ccdd9fa6ca8ac5eda57e4c9
YounesM <younes@mgharfaoui.com>
2017-12-05 23:45:37 +0000

    day 3 part 1

--- 2017/day3_1.js
+++ 2017/day3_1.js
@@ -1,8 +1,10 @@
-// --- Day 3: Spiral Memory (1/2) INCOMPLETE---
-let input = 1024, square_size = Math.ceil(Math.sqrt(input)), start_point;
+// --- Day 3: Spiral Memory (1/2) ---
+let input = 361527, square_size = Math.ceil(Math.sqrt(input)), start_point;
 square_size = square_size % 2 ? square_size : square_size + 1;
-start_point = Math.pow(square_size,2) - (square_size*4 -1);
-for(let i = start_point; i < Math.pow(square_size,2); i += square_size-1/2){
-    if(input > i)
-        console.log(input - i);
+start_point = Math.pow(square_size, 2) - ((square_size - 1) * 4 - 1);
+for (let i = start_point - 1; i <= Math.pow(square_size, 2); i += (square_size - 1)) {
+    if (input <= i) {
+        console.log(Math.abs(input - (i - ((square_size - 1) / 2))) + (square_size - 1) / 2);
+        break;
+    }
 }
