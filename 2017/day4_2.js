b036a14c225455a6cb99af5c572528eac0a32238
YounesM <younes@mgharfaoui.com>
2017-12-07 13:38:15 +0000

    Day 4 part 2

--- 2017/day4_2.js
+++ 2017/day4_2.js
@@ -1,9 +1,9 @@
-// --- Day 4: High-Entropy Passphrases (1/2)---
+// --- Day 4: High-Entropy Passphrases (2/2)---
 const fs = require('fs');
 let input = fs.readFileSync('input/2017/day4', 'utf8'), cnt = 0;
 input.split('\r\n').forEach(e => {
     let set = new Set();
-    e.split(' ').forEach(el => {set.add(el)});
+    e.split(' ').forEach(el => {set.add(el.split('').sort().join(''))});
     if(set.size === e.split(' ').length) cnt++;
 });
-console.log('PART 1: '+ cnt);
+console.log('PART 2: '+ cnt);
