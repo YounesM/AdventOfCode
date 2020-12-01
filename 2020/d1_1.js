84f1c4d7c62b7020beec07f62b1b4deddc16e7f5
YounesM <younes@mgharfaoui.com>
2020-12-01 12:24:51 +0100

    A little bit better

--- 2020/d1_1.js
+++ 2020/d1_1.js
@@ -1,7 +1,11 @@
 // --- Day 1: Report Repair --- (PART 1)
 const fs = require('fs'), input = fs.readFileSync('../input/2020/day1', 'utf8').split('\r\n');
-input.forEach(a => input.forEach(b => {
-    if (+a + +b === 2020) {
-        console.log(a*b);
-    }
-}));
+try {
+    input.forEach(a => input.forEach(b => {
+        let FoundException = {a: a, b: b}
+        if (+a + +b === 2020) throw FoundException
+    }));
+} catch (e) {
+    console.log(e.a*e.b)
+}
+
--- 2020/d1_2.js
+++ 2020/d1_2.js
@@ -1,7 +1,11 @@
 // --- Day 1: Report Repair --- (PART 2)
 const fs = require('fs'), input = fs.readFileSync('../input/2020/day1', 'utf8').split('\r\n');
-input.forEach(a => input.forEach(b => { input.forEach(c => {
-    if (+a + +b + +c === 2020) {
-        console.log(a*b*c);
-    }
-})}));
+try {
+    input.forEach(a => input.forEach(b => { input.forEach(c => {
+        let FoundException = {a: a, b: b, c: c}
+        if (+a + +b + +c === 2020) throw FoundException
+    })}));
+} catch (e) {
+    console.log(e.a*e.b*e.c)
+}
+
